62666053b27f3b30fee01323df09bd98946cfeaa
jonkofee <jonkofee@icloud.com>
2024-10-08 17:27:37 +0300

    fix url

--- internal/config/services.go
+++ internal/config/services.go
@@ -3,7 +3,7 @@ package config
 import "net/url"
 
 type ServicesConfig struct {
-	UsersEndpoint     url.URL `env:"GRPC_USERS_ENDPOINT,required" envDefault:"0.0.0.0:51052"`
-	CompaniesEndpoint url.URL `env:"GRPC_COMPANIES_ENDPOINT,required" envDefault:"0.0.0.0:51053"`
-	BotsEndpoint      url.URL `env:"GRPC_BOTS_ENDPOINT,required" envDefault:"0.0.0.0:51054"`
+	UsersEndpoint     url.URL `env:"GRPC_USERS_ENDPOINT,required" envDefault:"127.0.0.1:51052"`
+	CompaniesEndpoint url.URL `env:"GRPC_COMPANIES_ENDPOINT,required" envDefault:"127.0.0.1:51053"`
+	BotsEndpoint      url.URL `env:"GRPC_BOTS_ENDPOINT,required" envDefault:"127.0.0.1:51054"`
 }
